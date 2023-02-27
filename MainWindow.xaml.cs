50a9b1fddb8d3dbe0c166ffc596b86cd0e88c5b1
IrynaNakoneshniuk <103773315+IrynaNakoneshniuk@users.noreply.github.com>
2023-02-27 20:39:10 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GutenbergDownloader
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void DownloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri ("https://gutenberg.org");
+                var response = await client.GetAsync("https://www.gutenberg.org/ebooks/1524.txt.utf-8");
+                var html = await response.Content.ReadAsStringAsync();
+                this.tbText.Text = html;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка: {ex.Message}");
+            }
+        }
+    }
+}
+    
+
